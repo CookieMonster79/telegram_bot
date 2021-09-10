ba921bb626f6a59abd2e3507c1042941206d79ee
DESKTOP-6T0BCUM\XAOC <moskva-max@yandex.ru>
2021-09-10 13:10:22 +0300

    Добавлена проверка на отсутствие логина

--- Groovy Script/loginForEmpl.groovy
+++ Groovy Script/loginForEmpl.groovy
@@ -7,9 +7,10 @@ def loginForEmpl(names){
     names.each{
       usr=utils.find('employee',[title:op.like(it)])
       listNames = listNames + usr
+      listNames = listNames.findAll{it.login != null}
         listNames.each{
           if(usr){
-            keys+="${api.web.asLink(api.web.open(it, api.auth.getAccessKey(it.login).setReusable().setDeadlineHours(1)), it.title)} <pre>\n</pre>"
+            keys+="${api.web.asLink(api.web.open(it, api.auth.getAccessKey(it?.login).setReusable().setDeadlineHours(1)), it.title)} <pre>\n</pre>"
           } else{
             keys = null
             }
