e7ac462df1f74051ed21090f729621b4786317c8
DESKTOP-6T0BCUM\XAOC <moskva-max@yandex.ru>
2021-11-01 19:14:27 +0300

    Вернул запуск на 10 утра

--- bot.py
+++ bot.py
@@ -58,7 +58,7 @@ def start_command(message):
         return text
 
     def run():
-        scheduler.add_job(bot.send_message, trigger='cron', hour='18', minute='54',
+        scheduler.add_job(bot.send_message, trigger='cron', hour='10', minute='00',
                           args=[message.chat.id, approvDate()])
         scheduler.start()
 
